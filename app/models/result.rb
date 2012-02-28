3d70a64262e673895fe4dbe39b3a92608829ff71
Duncan Stuart <dgmstuart@gmail.com>
2012-02-28 20:46:56 +0000

    Results: Added a failure count and a "Close" link at the bottom

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,14 +1,23 @@
 class Result < ActiveRecord::Base
   belongs_to :test_file
   
+  #TODO should be in the helper? Didn't put it there initially on the assumption that this is useful in logic as well...
   def result_status
-    # Simulate random successes and failures:
-    if rand(5) == 0
+    if failures > 0
       "alert-error"
     else
       "alert-success"
     end
     # These are twitter bootstrap class names. I tried to define my own classes inheriting from these, but couldn't get it to work...
+  end
+  
+  def failures
+    # Simulate random successes and failures:
     
+    unless time_run.to_i.modulo(5)==0
+      0
+    else
+      rand(20) + 1
+    end
   end
 end
