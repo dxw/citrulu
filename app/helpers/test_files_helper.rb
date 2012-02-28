c95e325e1433b89c987d76e11d05d5ff366bd5f6
Duncan Stuart <dgmstuart@gmail.com>
2012-02-28 19:57:41 +0000

    Minor cleanups and improving console date display

--- app/helpers/test_files_helper.rb
+++ app/helpers/test_files_helper.rb
@@ -4,16 +4,11 @@ module TestFilesHelper
   def console_line(content, status = "neutral")
     line_items = []
     # line_items << content_tag(:span, "[#{Time.now}]", :class => "date")
-    line_items << "[#{Time.now}] "
+    line_items << "[#{Time.now.to_s(:db)}] "
     line_items << content_tag(:span, content, :class => status)
     content_tag :p, :escape => false do
       # line_items.join(" ")
       line_items.collect {|item| concat(item)}
     end
   end
-  
-  # # the message to be displayed when the file is saving
-  # def editor_status_message(content)
-  #   content_tag :div, content
-  # end  
 end
