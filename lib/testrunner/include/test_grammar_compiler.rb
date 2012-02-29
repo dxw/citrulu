5c545c59747e5c217ffaa71088611b673a10387a
Harry Metcalfe <harry@thedextrousweb.com>
2012-02-29 17:08:24 +0000

    Get structured data for error messages

--- lib/testrunner/include/test_grammar_compiler.rb
+++ lib/testrunner/include/test_grammar_compiler.rb
@@ -6,6 +6,17 @@ class Compiler
   
   class TestCompileError < StandardError
   end
+
+  def format_error(error)
+    matches = error.to_s.match(/^Failed to compile: Expected one of ((([^,]+,)+[^,]+) at) line (\d+), column (\d+) \(byte (\d+)\) after(.*)$/)
+
+    {
+      :expected => matches[2].split(', '),
+      :line => matches[4],
+      :column => matches[5],
+      :after => matches[7]
+    }
+  end
   
   def compile_tests(code)
     parser = TesterGrammarParser.new
