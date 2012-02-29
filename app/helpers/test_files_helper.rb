6bc7410e8e65109f0e35fe9118c01f517a463dbb
Harry Metcalfe <harry@thedextrousweb.com>
2012-02-29 17:16:03 +0000

    Reformatted a bit and added some comments

--- app/helpers/test_files_helper.rb
+++ app/helpers/test_files_helper.rb
@@ -3,11 +3,11 @@ module TestFilesHelper
   # a line to be displayed in the console element
   def console_line(content, status = "neutral")
     line_items = []
-    # line_items << content_tag(:span, "[#{Time.now}]", :class => "date")
+    
     line_items << "[#{Time.now.to_s(:db)}] "
     line_items << content_tag(:span, content, :class => status)
+
     content_tag :p, :escape => false do
-      # line_items.join(" ")
       line_items.collect {|item| concat(item)}
     end
   end
