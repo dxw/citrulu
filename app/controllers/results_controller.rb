319337f0a83cf256c1e2bed2e9995b4586769516
Duncan Stuart <dgmstuart@gmail.com>
2012-02-29 19:49:03 +0000

    Plumbing real Yaml into the Results page

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -3,6 +3,38 @@ class ResultsController < ApplicationController
   
   before_filter :authenticate_user!
   
+  require 'psych' 
+  
+  # for YAML parsing
+  
+  DUMMY_RESULTS_YAML = %{---
+- :test_url: http://harrymetcalfe.com
+  :tests:
+  - :assertion: :page_contain
+    :value: Harry's Home on the Web
+    :passed: true
+  - :assertion: :page_not_contain
+    :value: Harry's Home on the Web
+    :passed: false
+  - :assertion: :source_contain
+    :value: This is an automatically generated list of things that might have something to do with me
+    :passed: true
+  - :assertion: :page_not_contain
+    :value: PHP Error
+    :passed: true
+  - :assertion: :page_not_contain
+    :value: PHP Warning
+    :passed: true
+  - :assertion: :headers_include
+    :value: X-Varnish
+    :passed: false
+  - :assertion: :headers_not_include
+    :value: X-Varnish
+    :passed: true
+  :test_date: 2012-02-29 15:55:03.709027957 +00:00}
+  
+  
+    
   # GET /results
   # GET /results.json
   def index
@@ -16,21 +48,22 @@ class ResultsController < ApplicationController
     offset = 0
     while @results.length < 10
       offset += rand(100)
+            
+      # result_object = []
+      # number_of_pages = rand(3) + 5
+      # while result_object.length < number_of_pages
+      #   steps = []
+      #   number_of_steps = rand(3) + 3
+      #   while steps.length < number_of_steps
+      #     step = "This is a result" 
+      #     steps << step
+      #   end
+      #   result_object << ["http://thisisapage#{Time.now.to_i}", steps]
+      # end
+
+      result_yaml= DUMMY_RESULTS_YAML
       
-      
-      result_object = []
-      number_of_pages = rand(3) + 5
-      while result_object.length < number_of_pages
-        steps = []
-        number_of_steps = rand(3) + 3
-        while steps.length < number_of_steps
-          step = "This is a result" 
-          steps << step
-        end
-        result_object << ["http://thisisapage#{Time.now.to_i}", steps]
-      end
-      
-      @results << Result.new(:id => offset, :time_run => Time.now - offset, :result => result_object)
+      @results << Result.new(:id => offset, :time_run => Time.now - offset, :result => result_yaml)
     end
 ##END TEMPORARY CODE
 
