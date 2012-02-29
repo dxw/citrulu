62fb1364750246845062e2d85bef0a46afbcad42
Duncan Stuart <dgmstuart@gmail.com>
2012-02-29 20:36:51 +0000

    Improving presentation of Results list

--- app/models/result.rb
+++ app/models/result.rb
@@ -8,17 +8,20 @@ class Result < ActiveRecord::Base
     Psych.load(result)
   end
   
-  #TODO should be in the helper? Didn't put it there initially on the assumption that this is useful in logic as well...
-  def result_status
-    if failures > 0
-      "alert-error"
-    else
-      "alert-success"
-    end
-    # These are twitter bootstrap class names. I tried to define my own classes inheriting from these, but couldn't get it to work...
+  
+  def number_of_pages
+    result_object.length
   end
   
-  def failures
+  def number_of_checks
+    count = 0
+    result_object.each do | test_group |
+      count += test_group[:tests].length
+    end
+    return count
+  end
+  
+  def number_of_failures
     # Simulate random successes and failures:
     
     unless time_run.to_i.modulo(5)==0
@@ -27,4 +30,18 @@ class Result < ActiveRecord::Base
       rand(20) + 1
     end
   end
+  
+  
+  
+  #TODO should be in the helper? Didn't put it there initially on the assumption that this is useful in logic as well...
+  def result_status
+    if number_of_failures > 0
+      "alert-error"
+    else
+      "alert-success"
+    end
+    # These are twitter bootstrap class names. I tried to define my own classes inheriting from these, but couldn't get it to work...
+  end
+  
+  
 end
