eb03d6894469181c4d2a2a1fc8b5b68a30cbc8e6
Duncan Stuart <dgmstuart@gmail.com>
2012-03-05 15:02:11 +0000

    Backing out holding page - mistakenly checked into master

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,80 @@
 SimpleFrontEndTesting::Application.routes.draw do
-  match '*path' => redirect('/')
+
+  devise_for :users 
+  
+  devise_scope :user do
+    root :to => "website#index"
+  end
+  # The above SHOULD work, but might not work in production. The following is from see https://github.com/plataformatec/devise/wiki/How-To%3A-Redirect-to-a-specific-page-on-successful-sign-in-out
+  # match '/user' => "website#test_file_editor", :as => :user_root
+
+  resources :test_files
+  
+  resources :test_runs
+  
+  resources :test_groups
+  
+  resources :test_results
+    
+  # Assume everything else is a page on the website:
+  match ':action' => 'website', :as => "page"
+  
+  root :to => "website#index"
+  
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
+
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
+
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Sample resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
+  #   end
+
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => 'welcome#index'
+
+  # See how all your routes lay out with "rake routes"
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id(.:format)))'
 end
