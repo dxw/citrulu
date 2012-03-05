e2d941fcd1dbc3750b7e896a464bb003ddfa8a89
Harry Metcalfe <harry@thedextrousweb.com>
2012-03-05 15:40:20 +0000

    First stab at rake tasks which runs tests & adds results to the DB

--- lib/tasks/run_the_tests.rake
+++ lib/tasks/run_the_tests.rake
@@ -1,6 +1,8 @@
 require 'testrunner'
 
 desc "Run all the tests"
-task :run_tests do
-  
+task :run_tests => :environment do
+  runner = TestRunner.new
+
+  runner.run_all_tests
 end
--- lib/testrunner.rb
+++ lib/testrunner.rb
@@ -1,12 +1,57 @@
-require 'treetop'
-require 'grammar/grammar'
+require 'grammar/parser'
+require 'grammar/predefs'
+require 'mechanize'
 
 class TestRunner
-  def execute_testfile
-    agent = Mechanize.new
 
+  def parser
+    @parser ||= CitruluParser.new
+    @parser
+  end
+
+  def run_all_tests
+    TestFile.find(:all).each do |file|
+      test_run = TestRun.new
+      test_run.time_run = Time.now
+      test_run.test_file_id = Time.now
+      test_run.save
+
+      results = execute_tests(parser.compile_tests(file.compiled_test_file_text))
+
+      results.each do |result|
+        test_group = TestGroup.new
+        test_group.test_run_id = test_run.id
+        test_group.time_run = result[:test_date]
+        test_group.save
+
+        result[:tests].each do |test|
+          test_result = TestResult.new
+          test_result.test_group_id = test_group.id
+          test_result.assertion = test[:assertion]
+          test_result.value = test[:value]
+          test_result.name = test[:name]
+          test_result.result = test[:passed]
+          test_result.save
+        end
+      end
+    end
+  end
+
+  def do_test(testvalues, &block)
+    passed = false
+
+    testvalues.each do |value|
+      passed = yield(value)
+
+      return passed if !passed
+    end
+
+    passed
+  end
+
+  def execute_tests(tests)
     tests.each do |group|
-      puts "Running tests for #{group[:test_url]}..."
+      agent = Mechanize.new
 
       group[:test_date] = Time.now
 
@@ -21,24 +66,45 @@ class TestRunner
       end
 
       group[:tests].each do |test|
-      case test[:assertion]
-      when :page_contain
-        test[:passed] = page.root.inner_text.match(test[:value]) != nil
+ 
+      testvalues = []
 
-      when :page_not_contain
-        test[:passed] = page.root.inner_text.match(test[:value]) == nil
+      if test[:value].blank? && !test[:name].blank?
+        testvalues += Predefs.find(test[:name])
+      else
+        testvalues << test[:value]
+      end
+
+      case test[:assertion]
+      when :i_see
+        test[:passed] = do_test(testvalues) do |value|
+          page.root.inner_text.match(value) != nil
+        end
+
+      when :i_not_see
+        test[:passed] = do_test(testvalues) do |value|
+          page.root.inner_text.match(value) == nil
+        end
 
       when :source_contain
-        test[:passed] = page.content.match(test[:value]) != nil
+        test[:passed] = do_test(testvalues) do |value|
+          page.content.match(value) != nil
+        end
 
       when :source_not_contain
-        test[:passed] = page.content.match(test[:value]) == nil
+        test[:passed] = do_test(testvalues) do |value|
+          page.content.match(value) == nil
+        end
 
       when :headers_include
-        test[:passed] = page.header.include?(test[:value])
+        test[:passed] = do_test(testvalues) do |value|
+          page.header.include?(value)
+        end
 
       when :headers_not_include
-        test[:passed] = !page.header.include?(test[:value])
+        test[:passed] = do_test(testvalues) do |value|
+          !page.header.include?(value)
+        end
 
       else
         test[:message] = "Internal error: no such test' #{test[:assertion]}"
