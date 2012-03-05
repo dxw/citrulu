17996ff6b4d108cc27d6ce793f4f5f217d4eb48e
Harry Metcalfe <harry@thedextrousweb.com>
2012-03-05 19:01:21 +0000

    Added response code to test group

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120305121325) do
+ActiveRecord::Schema.define(:version => 20120305183947) do
 
   create_table "results", :force => true do |t|
     t.integer  "test_file_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(:version => 20120305121325) do
     t.time     "time_run"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "response_code"
   end
 
   create_table "test_results", :force => true do |t|
