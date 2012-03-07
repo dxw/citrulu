4980e27f09df7232ae2f2f2b4cc1c79e21d495b5
Duncan Stuart <dgmstuart@gmail.com>
2012-03-07 02:03:53 +0000

    Extending symbolizer to work on test strings

--- lib/grammar/symbolizer.rb
+++ lib/grammar/symbolizer.rb
@@ -2,8 +2,13 @@ class String
   def to_test_sym
     self.strip.gsub(/should /, '').gsub(/\s+/, '_').downcase.to_sym
   end
+  
+  def to_test_s
+    self.to_s.sub(/_/," should ").gsub(/_/, ' ').capitalize
+  end
 end
 
+# Not used...
 class Symbol
   def to_test_s
     self.to_s.sub(/_/," should ").gsub(/_/, ' ').capitalize
