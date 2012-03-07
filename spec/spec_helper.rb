8817386712e111dfe276ed8c35936d330eecb796
Harry Metcalfe <harry@thedextrousweb.com>
2012-03-07 21:48:35 +0000

    Use simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/config/'
+  add_filter '/vendor/'
+end
+
 require 'spork'
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
