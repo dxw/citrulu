5190cd4f4b1ad2949bbe05a4616824e34ee22582
Harry Metcalfe <harry@thedextrousweb.com>
2012-03-07 20:06:24 +0000

    Removed old status helper

--- app/models/test_run.rb
+++ app/models/test_run.rb
@@ -28,12 +28,4 @@ class TestRun < ActiveRecord::Base
   
   
   #TODO should be in the helper? Didn't put it there initially on the assumption that this is useful in logic as well...
-  def status
-    if number_of_failures > 0
-      "alert-error"
-    else
-      "alert-success"
-    end
-    # These are twitter bootstrap class names. I tried to define my own classes inheriting from these, but couldn't get it to work...
-  end
 end
