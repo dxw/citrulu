8bd5d3df49bdcab1b624b7b3b7968c8640729b12
Harry Metcalfe <harry@thedextrousweb.com>
2012-03-08 19:10:09 +0000

    Moar tests

--- spec/controllers/test_runs_controller_spec.rb
+++ spec/controllers/test_runs_controller_spec.rb
@@ -12,15 +12,36 @@ describe TestRunsController do
   
   describe "GET index" do
     # it "assigns all test_runs as @test_runs" do
-    it "gets the (single) test file from the current user and returns the list of test runs as @test_runs" do
-      pending "Integration of the new index page"
-      test_run = FactoryGirl.create(:test_run, :test_file => @user.test_files[0])
+    it "should define @test_files" do
       get :index
-      assigns(:test_runs).should eq([test_run]) 
+      assigns(:test_files).should be_an(Array) 
+    end
+    it "should define @test_runs" do
+      get :index
+      assigns(:test_runs).should be_an(Array) 
+    end
+
+    it "should define @recent_failed_test_groups" do
+      get :index
+      assigns(:recent_failed_groups).should be_an(Array) 
     end
     
-    # Once We've actually implemented test runs....
-    it "only shows test runs which belong to the current user" 
+    it "should render the index template" do
+      get :index
+
+      response.should render_template("index")
+    end
+
+    it "only shows test runs which belong to the current user" do 
+      another_user = FactoryGirl.create(:user)
+
+      get :index
+
+      file_users = assigns(:test_files).collect{|f| f.user_id}.uniq
+
+      file_users.should have(1).items
+      file_users.first.should == controller.current_user.id
+    end
   end
 
   describe "GET show" do
