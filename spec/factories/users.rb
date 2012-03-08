409b4484b1fd8268d696931b1c3b983df2cf978d
Harry Metcalfe <harry@thedextrousweb.com>
2012-03-08 19:17:25 +0000

    Added invititation code to user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,6 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "person#{n}@dxw.com" }
     sequence(:password) { |m| "P@55w0rd#{m}" }
+    sequence(:invitation_code) { |m| "4ec364d986d" }
   end
 end
