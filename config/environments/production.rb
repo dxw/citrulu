21880d48b2a0b4a70cc9c26bc568a14abad9121e
Harry Metcalfe <harry@thedextrousweb.com>
2012-03-15 17:45:21 +0000

    Default host for actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,8 @@ SimpleFrontEndTesting::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = {
+    :host => 'http://citrulu.com'
+  }
 end
