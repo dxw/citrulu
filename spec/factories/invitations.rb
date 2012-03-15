de48ee7df73b62d81c6d7b3c11bb2fe7676429bb
Duncan Stuart <dgmstuart@gmail.com>
2012-03-15 15:03:16 +0000

    Fixing most of the tests which were breaking due to invitation code validation

---
+++ spec/factories/invitations.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :invitation do
+    code    "testcode123"
+    enabled true
+  end
+end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -2,7 +2,7 @@ module ControllerMacros
   def login_user
     before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      @user = Factory.create(:user)
+      @user = Factory.build(:user)
       @user.confirm!
       sign_in @user
     end
