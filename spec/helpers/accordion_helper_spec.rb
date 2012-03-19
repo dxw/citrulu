1787bc5f8cdadb9ae4c3020f7a3a8b0355b44e2d
Harry Metcalfe <harry@thedextrousweb.com>
2012-03-19 15:08:58 +0000

    Basic test for the accordion helper

--- spec/helpers/accordion_helper_spec.rb
+++ spec/helpers/accordion_helper_spec.rb
@@ -7,28 +7,30 @@ require 'spec_helper'
 
 describe AccordionHelper do
   it "Returns a non-empty string and doesn't throw an exception" do
-    accordion = helper.accordion('test') do
-      group = helper.accordion_group('test_group') do
-        heading = helper.accordion_heading do
-          'Heading'
+    lambda do
+      accordion = helper.accordion('test') do
+        group = helper.accordion_group('test_group') do
+          heading = helper.accordion_heading do
+            'Heading'
+          end
+
+          heading.should be_a(String)
+          heading.should_not be_empty
+
+          body = helper.accordion_body do
+            'Body'
+          end
+
+          body.should be_a(String)
+          body.should_not be_empty
         end
 
-        heading.should be_a(String)
-        heading.should_not be_empty
-
-        body = helper.accordion_body do
-          'Body'
-        end
-
-        body.should be_a(String)
-        body.should_not be_empty
+        group.should be_a(String)
+        group.should_not be_empty
       end
 
-      group.should be_a(String)
-      group.should_not be_empty
-    end
-
-    accordion.should be_a(String)
-    accordion.should_not be_empty
+      accordion.should be_a(String)
+      accordion.should_not be_empty
+    end.should_not raise_error
   end
 end
