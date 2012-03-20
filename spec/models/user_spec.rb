10ae6a80a75c1bd6e3f27e3068b2142075465267
Duncan Stuart <dgmstuart@gmail.com>
2012-03-20 23:23:57 +0000

    Completing coverage on user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,25 @@
 require 'spec_helper'
 
 describe User do
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+  end
+  
+  
   it "should add the default test file when it's created" do
-    user = FactoryGirl.create(:user)
-    user.test_files.first.test_file_text.should == DEFAULT_TEST_FILE
+    @user.test_files.first.test_file_text.should == DEFAULT_TEST_FILE
   end
   
   it "should set the email preference to recieve test run emails when it's created" do
-    user = FactoryGirl.create(:user)
-    user.email_preference.should == 1
+    @user.email_preference.should == 1
+  end
+  
+  describe ".send_welcome_email" do
+    it "should create a welcome email for the current user and deliver it" do
+      UserMailer.should_receive(:welcome_email).with(@user).and_return(Mail::Message.new)
+      Mail::Message.any_instance.should_receive(:deliver)
+    
+      @user.send_welcome_email
+    end
   end
 end
