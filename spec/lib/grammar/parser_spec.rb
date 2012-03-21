c943b8cf6ac0dbb131e5c9fab87d5416f2e9f4e9
Harry Metcalfe <harry@thedextrousweb.com>
2012-03-21 11:56:58 +0000

    Took out quotes from test data

--- spec/lib/grammar/parser_spec.rb
+++ spec/lib/grammar/parser_spec.rb
@@ -9,29 +9,29 @@ describe CitruluParser do
     COMPILER_OUTPUT = [
       {:test_url=>"http://www.google.com", 
         :tests=>[
-          {:assertion=>:i_see, :original_line =>  "  I should see 'are you feeling lucky?'\n", :value=>"'are you feeling lucky?'"},
-          {:assertion=>:i_not_see, :original_line => "  I should not see 'are you feeling Stupid?'\n", :value=>"'are you feeling Stupid?'"}
+          {:assertion=>:i_see, :original_line =>  "  I should see are you feeling lucky?\n", :value=>"are you feeling lucky?"},
+          {:assertion=>:i_not_see, :original_line => "  I should not see are you feeling Stupid?\n", :value=>"are you feeling Stupid?"}
         ]
       }
     ]
 
     it "should handle input with an initial comment" do
-      code = "#this is some code beginning with a comment\nOn http://www.google.com\n  I should see 'are you feeling lucky?'\n  I should not see 'are you feeling Stupid?'"
+      code = "#this is some code beginning with a comment\nOn http://www.google.com\n  I should see are you feeling lucky?\n  I should not see are you feeling Stupid?"
       CitruluParser.new.compile_tests(code).should == COMPILER_OUTPUT
     end
     
     it "should handle input with a comment after the On line" do
-      code = "On http://www.google.com\n#a comment after the first line\n  I should see 'are you feeling lucky?'\n  I should not see 'are you feeling Stupid?'"
+      code = "On http://www.google.com\n#a comment after the first line\n  I should see are you feeling lucky?\n  I should not see are you feeling Stupid?"
       CitruluParser.new.compile_tests(code).should == COMPILER_OUTPUT
     end
     
     it "should handle input with a comment between two checks" do
-      code = "On http://www.google.com\n  I should see 'are you feeling lucky?'\n# this is a comment between two checks \n  I should not see 'are you feeling Stupid?'"
+      code = "On http://www.google.com\n  I should see are you feeling lucky?\n# this is a comment between two checks \n  I should not see are you feeling Stupid?"
       CitruluParser.new.compile_tests(code).should == COMPILER_OUTPUT
     end
     
     it "should handle input with a closing comment" do
-      code = "On http://www.google.com\n  I should see 'are you feeling lucky?'\n  I should not see 'are you feeling Stupid?'\n#   ending with a comment  " 
+      code = "On http://www.google.com\n  I should see are you feeling lucky?\n  I should not see are you feeling Stupid?\n#   ending with a comment  " 
       CitruluParser.new.compile_tests(code).should == COMPILER_OUTPUT
     end
 
