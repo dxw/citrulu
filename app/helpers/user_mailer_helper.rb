aeec722a68e5ee5c0b77511c3c5921879569b51e
Duncan Stuart <dgmstuart@gmail.com>
2012-03-28 16:09:20 +0100

    Putting more content in the welcome email and prettifying it (inc. adding a button)

--- app/helpers/user_mailer_helper.rb
+++ app/helpers/user_mailer_helper.rb
@@ -14,6 +14,14 @@ module UserMailerHelper
     ""
   end
   
+  # Buttons are a td with a background colour and coloured borders, containing a link 
+  def content_button_td_styles
+    "padding: 5px 10px; background: #08C; border: 1px solid #002A80; border-color: #3CF #002A80 #002A80 #3CF;"
+  end
+  def content_button_a_styles  
+    "display: block; text-decoration: none; color: #fff!important; font-weight: bold; font-size: 20px!important; white-space:nowrap;"
+  end
+  
   def content_pre_styles
     "font-size:0.9em; margin-left:15px; padding: 2px 5px; background: #fff; border:1px solid #000; -webkit-border-radius: 3px; -moz-border-radius-topright: 3px; border-radius: 3px;"
   end  
@@ -24,7 +32,7 @@ module UserMailerHelper
   end
 
   def footer_a_styles
-    "color:fff;"
+    "color:#fff;"
   end
     
 end
