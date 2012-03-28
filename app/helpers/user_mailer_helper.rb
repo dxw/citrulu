aadc57b0287f6ee9fc587897480c324076fa2207
Duncan Stuart <dgmstuart@gmail.com>
2012-03-28 13:20:07 +0100

    Making all styles inline in Emails and improving link styles

--- app/helpers/user_mailer_helper.rb
+++ app/helpers/user_mailer_helper.rb
@@ -4,4 +4,27 @@ module UserMailerHelper
   def failed_groups_header
     "The following #{pluralize(@test_run.number_of_failed_groups, 'page')} could not be retrieved because of errors:"
   end
+  
+  
+  def content_p_styles
+    "line-height: 16px; margin: 0 0 10px 0;"
+  end
+  
+  def content_a_styles
+    ""
+  end
+  
+  def content_pre_styles
+    "font-size:0.9em; margin-left:15px; padding: 2px 5px; background: #fff; border:1px solid #000; -webkit-border-radius: 3px; -moz-border-radius-topright: 3px; border-radius: 3px;"
+  end  
+  
+  # Not currently used
+  def content_hn_styles 
+    "font-size: 18px; font-weight: bold; margin: 10px 0 3px 0;"
+  end
+
+  def footer_a_styles
+    "color:fff;"
+  end
+    
 end
