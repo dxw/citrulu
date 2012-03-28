a8ab5a1df6332befa06ecf813c7a22973083d453
Duncan Stuart <dgmstuart@gmail.com>
2012-03-28 03:41:20 +0100

    Factoring out @failures, adding a different title for when groups have failed but no tests have failed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -23,11 +23,14 @@ class UserMailer < ActionMailer::Base
   def test_notification_failure(test_run)
     raise "Tried to create a test notification for a nil test run" if test_run.nil?
     raise "Tried to create a test notification for a test test_run with no groups: id#{test_run.id}" if test_run.test_groups.nil?
-    
-    @failures = test_run.test_groups.sum{|x|x.test_results.select{|result| !result.result}.count}
-    
+        
     @status = :fail
-    @title = "#{@failures} of your tests just failed"
+    if test_run.has_failed_tests?
+      @title = "#{test_run.number_of_failed_tests} of your tests just failed"
+      # Some pages may have failed in this case as well, but we want to keep the title short
+    else #test_run.has_failed_groups?
+      @title = "#{test_run.number_of_failed_groups} pages could not be retrieved"
+    end
     
     # TODO - DRY These up - couldn't work out how to do it!
     @test_run = test_run
