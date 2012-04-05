83b10f6dd8a8671026f3914507a43ff65ea9c8e4
Duncan Stuart <dgmstuart@gmail.com>
2012-04-05 15:23:19 +0100

    Configuring rspreedly

---
+++ config/initializers/rspreedly_config.rb
@@ -0,0 +1,7 @@
+# Cornichon ID = 18462 
+# Gherkin ID = 18464
+
+RSpreedly::Config.setup do |config|
+  config.api_key        = "2d8046e6407e97adcbe8a19e7ab2a22c46a5cfbb"
+  config.site_name      = "dgmstuart-test"
+end
