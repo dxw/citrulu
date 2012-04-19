7ae672be71e64e7da0c423e134425c44953607b8
Harry Metcalfe <harry@thedextrousweb.com>
2012-04-19 20:24:49 +0100

    Updated test file template

--- config/initializers/test_file_templates.rb
+++ config/initializers/test_file_templates.rb
@@ -1,33 +1,77 @@
 DEFAULT_TEST_FILE=%{# Welcome to your first test file!
 
 # This file tests example.com - you should change it so that it checks your 
-# own website. We should shortly start running these tests! You can 
-# check the results on the results tab above.
+# own website. We will shortly start running these tests.
 
-# A test file consists of any number of test groups.
-# Each test group contains checks on a specific URL: 
+# After your tests have run, you can check the results by following the link
+# in the menu at the top. While you're writing your tests, the results for
+# the page that you're on are displayed in the live view, over there --->
+
+# A test file consists of a number of requests for pages, followed by 
+# assertions that check what the request returned.
 On http://example.com
   # There are a number of different checks you can do. 
-  # This one checks for the text "we maintain a number of domains" 
-  # anywhere on the page:
+  # This one checks for the text "we maintain a number of domains" anywhere
+  # on the page:
   I should see we maintain a number of domains 
-  # N.B. Citrulu is case sensitive, so make sure your text matches exactly!
+
+  # Citrulu's green keywords are case sensitive, but the values you specify
+  # aren't (unless you're using a regular expression).
   
   # You can also check for the absence of text:
   I should not see A horrible error has occurred!
   
-  # This check looks for specific text in your source code:
+  # This assertion looks for specific text in your source code:
   Source should contain <img src="/_img/iana-logo-pageheader.png
   Source should not contain <img src="images/failwhale.gif 
+
+  # You can also write assertions that use regular expressions. You can use a
+  # regular expression anywhere where you're specifying a value
+  I should see /EXAMPLE\.(COM|ORG)/
+
+  # And if your fingers type quotes without your brain noticing, you can 
+  # use those too:
+  I should see "EXAMPLE.COM"
   
-  # This check retrieves the headers of a page and looks for a header with
+  # This assertion retrieves the headers of a page and looks for a header with
   # the name you specify:
   Headers should include server
   Headers should not include X-Varnish
+
+  # You can also check the contents of headers:
+  Header Server should contain Apache
+  Header Content-Type should contain /UTF-8$/
   
-  # Predefines are lists of common values which you may want to check for.
-  # You can see a list of these at the bottom of the help section:
-  I should not see :php_errors
+# It's good to write tests for specific things, so that you know what broke if 
+# the test fails. You can add "So" clauses to remind you what you're looking for:
+So that I know the page menu is being displayed
+On http://www.iana.org/domains/example/
+  Source should contain <div id="header-nav">
+
+# "On..." is really just a shortcut for "When I get...". This syntax lets you use
+# other HTTP methods, and also lets you add data:
+So that I know the server will respond to POSTs
+When I post "variable=value" to http://www.iana.org/domains/example/
+  I should see "Example Domains"
+
+# HEAD requests are useful when you only want to check headers. DELETE and GET have
+# the same syntax:
+When I head http://example.com
+  Response code should be 302
+  Header Server should contain BigIP
+
+# "When I..." supports the HEAD, GET, POST, PUT and DELETE methods.
+
+# You can also check what response code was returned. If you don't add a
+# check like this, Citrulu will assume that you want a 200 response, and
+# will add a check for you. But you can specify them yourself:
+So that I know custom 404s work
+When I get http://www.iana.org/domains/example/this-page-no-existy
+  Response code should be 404
+
+# The next bit is for complicated tests, so you might not need to read it 
+# now. You can remind yourself about anything you've read by using the Help
+# section on the right-hand side.
 
 # Setup and Teardown
 # ------------------
