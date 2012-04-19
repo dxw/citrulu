9e947f2f755abe3bac80105343e94d8d1aa47b01
Harry Metcalfe <harry@thedextrousweb.com>
2012-04-19 19:29:32 +0100

    Tests

--- spec/factories/test_results.rb
+++ spec/factories/test_results.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :test_result, :aliases => [:successful_test_result] do
-    assertion "i_see"
-    value "foobar"
+    original_line "I should see foobar"
     result true
     
     factory :failed_test_result do
