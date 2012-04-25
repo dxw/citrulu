1393434ad6fe9a9c89390307de6a337a8ad21ebc
Harry Metcalfe <harry@thedextrousweb.com>
2012-04-25 18:36:11 +0100

    Add sign in count and invitation description to user admin index

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -3,6 +3,10 @@ ActiveAdmin.register User do
     column :email
     column :unconfirmed_email
     column :email_preference
+    column :sign_in_count
+    column :invitation do |user|
+      div user.invitation.description
+    end
     column :current_sign_in_at
     column :last_sign_in_at
     column :current_sign_in_ip
