10322b5f02fe1ef241a0f8bce13cb9b376b154e8
Duncan Stuart <duncan@dxw.com>
2012-05-09 19:25:18 +0100

    Fixing the last of the broken specs which aren't actual bugs

--- spec/lib/grammar/parser_spec.rb
+++ spec/lib/grammar/parser_spec.rb
@@ -6,33 +6,31 @@ describe CitruluParser do
       expect { CitruluParser.new.compile_tests(nil) }.to raise_error(ArgumentError)
     end
     
-    COMPILER_OUTPUT = [
-      {:test_url=>"http://www.google.com", 
-        :tests=>[
-          {:assertion=>:i_see, :original_line =>  "I should see are you feeling lucky?", :value=>"are you feeling lucky?"},
-          {:assertion=>:i_not_see, :original_line => "I should not see are you feeling Stupid?", :value=>"are you feeling Stupid?"}
-        ]
-      }
+    COMPILER_OUTPUT_TESTS = [
+      {:assertion=>:response_code_be, :value=>"200", :original_line=>"Response code should be 200 after redirects", :value=>"200"},
+      {:assertion=>:i_see, :original_line =>  "I should see are you feeling lucky?", :value=>"are you feeling lucky?"},
+      {:assertion=>:i_not_see, :original_line => "I should not see are you feeling Stupid?", :value=>"are you feeling Stupid?"}
     ]
+      
 
     it "should handle input with an initial comment" do
       code = "#this is some code beginning with a comment\nOn http://www.google.com\n  I should see are you feeling lucky?\n  I should not see are you feeling Stupid?"
-      CitruluParser.new.compile_tests(code).should == COMPILER_OUTPUT
+      CitruluParser.new.compile_tests(code).first[:tests].should == COMPILER_OUTPUT_TESTS
     end
     
     it "should handle input with a comment after the On line" do
       code = "On http://www.google.com\n#a comment after the first line\n  I should see are you feeling lucky?\n  I should not see are you feeling Stupid?"
-      CitruluParser.new.compile_tests(code).should == COMPILER_OUTPUT
+      CitruluParser.new.compile_tests(code).first[:tests].should == COMPILER_OUTPUT_TESTS
     end
     
     it "should handle input with a comment between two checks" do
       code = "On http://www.google.com\n  I should see are you feeling lucky?\n# this is a comment between two checks \n  I should not see are you feeling Stupid?"
-      CitruluParser.new.compile_tests(code).should == COMPILER_OUTPUT
+      CitruluParser.new.compile_tests(code).first[:tests].should == COMPILER_OUTPUT_TESTS
     end
     
     it "should handle input with a closing comment" do
       code = "On http://www.google.com\n  I should see are you feeling lucky?\n  I should not see are you feeling Stupid?\n#   ending with a comment  " 
-      CitruluParser.new.compile_tests(code).should == COMPILER_OUTPUT
+      CitruluParser.new.compile_tests(code).first[:tests].should == COMPILER_OUTPUT_TESTS
     end
     
     it "should inject an assertion checking for a response code of 200" do
