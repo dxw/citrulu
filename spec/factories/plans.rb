c2753a0fb0f452e0bd026058620748afe2ce1136
Duncan Stuart <duncan@dxw.com>
2012-06-21 15:11:55 +0100

    Updating Plans Factory to reflect removal of Features

--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -7,11 +7,6 @@ FactoryGirl.define do
     test_frequency 86400
     number_of_sites 3
     mobile_alerts_allowance 12
-    
-    allows_custom_predefines false
-    allows_retrieved_pages false
-    allows_git_support false
-    allows_tests_on_demand false
 
     default false
     sequence(:spreedly_id) { |m| m }
