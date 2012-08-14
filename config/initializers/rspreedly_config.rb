9035fac26bdf0de545b676da2bb2d71098cbe2bb
Duncan Stuart <duncan@dxw.com>
2012-08-14 19:35:08 +0100

    Fixing a bug on the confirmation screen and updating the rspreedly config to point at prod

--- config/initializers/rspreedly_config.rb
+++ config/initializers/rspreedly_config.rb
@@ -2,6 +2,10 @@
 # Gherkin ID = 18464
 
 RSpreedly::Config.setup do |config|
-  config.api_key        = "2d8046e6407e97adcbe8a19e7ab2a22c46a5cfbb"
-  config.site_name      = "dgmstuart-test"
+  # config.api_key        = "2d8046e6407e97adcbe8a19e7ab2a22c46a5cfbb"
+  # config.site_name      = "dgmstuart-test"
+  
+  # Production:
+  config.api_key    = "290336bdd75acd2edc793e3b89d9945052349e8f"
+  config.site_name  = "Citrulu"
 end
