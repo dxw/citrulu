4df5871e3b0bbc7831cee9118633cad333926ca9
Duncan Stuart <duncan@dxw.com>
2012-09-04 11:51:25 +0100

    Configuring Rspreedly api key based on environment (prod or dev)

--- config/initializers/rspreedly_config.rb
+++ config/initializers/rspreedly_config.rb
@@ -2,10 +2,12 @@
 # Gherkin ID = 18464
 
 RSpreedly::Config.setup do |config|
-  # config.api_key        = "2d8046e6407e97adcbe8a19e7ab2a22c46a5cfbb"
-  # config.site_name      = "dgmstuart-test"
   
-  # Production:
-  config.api_key    = "290336bdd75acd2edc793e3b89d9945052349e8f"
-  config.site_name  = "Citrulu"
+  if Rails.env == 'production'
+    config.api_key    = "290336bdd75acd2edc793e3b89d9945052349e8f"
+    config.site_name  = "Citrulu"
+  else
+    config.api_key        = "2d8046e6407e97adcbe8a19e7ab2a22c46a5cfbb"
+    config.site_name      = "dgmstuart-test"
+  end
 end
