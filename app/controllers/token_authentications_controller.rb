56527d55a80b56076ff55313675767212627d636
Harry Metcalfe <harry@thedextrousweb.com>
2012-09-24 18:52:07 +0100

    Made this work for Citrulu

--- app/controllers/token_authentications_controller.rb
+++ app/controllers/token_authentications_controller.rb
@@ -3,16 +3,14 @@
 class TokenAuthenticationsController < ApplicationController 
 
   def create
-    @user = User.criteria.id(params[:user_id]).first
-    @user.reset_authentication_token!
-    redirect_to edit_user_registration_path(@user)
+    current_user.reset_authentication_token!
+    redirect_to edit_user_registration_path + '#api_key'
   end
 
   def destroy
-    @user = User.criteria.id(params[:id]).first
-    @user.authentication_token = nil
-    @user.save
-    redirect_to edit_user_registration_path(@user)
+    current_user.authentication_token = nil
+    current_user.save
+    redirect_to edit_user_registration_path + '#api_key'
   end
 
 end
